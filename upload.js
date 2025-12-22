6a93f875e029bad78a4baf661a8796a86b3cd659
anonymous <shyeyian@petalmail.com>
2025-12-22 10:08:32 +0800

    update

--- upload.js
+++ upload.js
@@ -1,6 +1,7 @@
 let child_process = require("child_process")
 let fs = require("fs")
 
+child_process.execSync("git pull")
 try {
     child_process.execSync("git add .")
     child_process.execSync("git commit -m update")
@@ -8,6 +9,5 @@ try {
 catch (error) { 
     // pass
 }
-
 child_process.execSync(`vsce publish patch --pat ${fs.readFileSync("vsce-token.txt")}`)
 child_process.execSync("git push")
